aee4f9a4a3d00559f1be55cb55f2aae8a38ef131
meliaph-monitech <meliaph.monitech@gmail.com>
2025-07-22 14:27:42 +0900

    Update 250722_CurveFitting_LowPassFilter_V03.py

--- 250722_CurveFitting_LowPassFilter_V03.py
+++ 250722_CurveFitting_LowPassFilter_V03.py
@@ -87,9 +87,15 @@ if uploaded_file is not None:
             selected_bead_idx = int(selected_bead_idx) - 1
             interval = st.slider("Curve Fitting Interval", 3, 101, 15, step=2)
             filter_type = st.selectbox("Low-pass Filter Type", ["Butterworth", "Moving Average", "Savitzky-Golay"])
-            cutoff = st.slider("Butterworth Cutoff Frequency", 0.01, 0.49, 0.1)
-            order = st.slider("Butterworth/Savgol Order", 1, 10, 3)
-            ma_window = st.slider("Moving Average Window", 3, 101, 15, step=2)
+            
+            if filter_type == "Butterworth":
+                cutoff = st.slider("Cutoff Frequency", 0.01, 0.49, 0.1)
+                order = st.slider("Filter Order", 1, 10, 3)
+            elif filter_type == "Moving Average":
+                ma_window = st.slider("Moving Average Window", 3, 101, 15, step=2)
+            elif filter_type == "Savitzky-Golay":
+                sg_window = st.slider("Savgol Window Length", 3, 101, 15, step=2)
+                sg_polyorder = st.slider("Savgol Polyorder", 1, 10, 3)
 
         # Precompute filtered signals for all beads
         bead_results = []
@@ -108,8 +114,9 @@ if uploaded_file is not None:
                 nir_filter = moving_average(nir, ma_window)
                 vis_filter = moving_average(vis, ma_window)
             elif filter_type == "Savitzky-Golay":
-                nir_filter = savgol_filter(nir, window_length=ma_window if ma_window % 2 else ma_window+1, polyorder=order)
-                vis_filter = savgol_filter(vis, window_length=ma_window if ma_window % 2 else ma_window+1, polyorder=order)
+                sg_window = sg_window if sg_window % 2 else sg_window + 1
+                nir_filter = savgol_filter(nir, window_length=sg_window, polyorder=sg_polyorder)
+                vis_filter = savgol_filter(vis, window_length=sg_window, polyorder=sg_polyorder)
 
             bead_results.append({"nir_raw": nir, "vis_raw": vis, "nir_curve": nir_curve, "vis_curve": vis_curve, "nir_filter": nir_filter, "vis_filter": vis_filter})
 
